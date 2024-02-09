19db7f55ce78de041a600b4c422177226c4f5100
baalais <99286765+baalais@users.noreply.github.com>
2024-02-09 10:57:56 +0200

    Playwright
    pp

---
+++ tests/birthdays.test.js
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test';
+
+test("list of birthdays on route /birthdays", async ({page}) => {
+    await page.goto("/birthdays");
+    
+    await expect(page.getByText("Hercules")).toBeVisible();
+    await expect(page.getByText("Athena")).toBeVisible();
+});
