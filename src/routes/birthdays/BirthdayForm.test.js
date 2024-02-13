78270950a5e3af1299d3e166d4a5cfe677ec9f3e
baalais <99286765+baalais@users.noreply.github.com>
2024-02-13 11:29:20 +0200

    sdzadsfad
    asdf

---
+++ src/routes/birthdays/BirthdayForm.test.js
@@ -0,0 +1,17 @@
+import { describe, it, expect} from 'vitest';
+import {render, screen} from "@testing-library/svelte";
+import BirthdayForm from "./BirthdayForm.svelte";
+
+describe ("BirthdayForm", () => {
+    it("has a form element", () => {
+        render(BirthdayForm);
+        expect(screen.queryByRole('form')).toBeVisible();
+    });
+    it("has a method post", () => {
+        render(BithdayForm);
+        expect(screen
+            .queryByRole("form")
+            .getAttribute("method"))
+        .toBe('post');
+    })
+});
