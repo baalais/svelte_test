3d32186fcae6077acc728b7fbc6db6325fd4e9db
baalais <99286765+baalais@users.noreply.github.com>
2024-02-16 12:16:38 +0200

    oij
    oij

--- src/routes/birthdays/BirthdayForm.svelte
+++ src/routes/birthdays/BirthdayForm.svelte
@@ -1,1 +1,11 @@
-<form name="birthdays" method="post"></form>
+<form name="birthdays" method="post">
+	<label>
+		Name
+		<input type="text" />
+	</label>
+	<label>
+		dob
+		<input type="date" />
+	</label>
+	<button>Save</button>
+</form>
--- src/routes/birthdays/BirthdayForm.test.js
+++ src/routes/birthdays/BirthdayForm.test.js
@@ -8,10 +8,28 @@ describe ("BirthdayForm", () => {
         expect(screen.queryByRole('form')).toBeVisible();
     });
     it("has a method post", () => {
-        render(BithdayForm);
+        render(BirthdayForm);
         expect(screen
             .queryByRole("form")
             .getAttribute("method"))
         .toBe('post');
-    })
+    });
+    it("has a submit button", () => {
+        render(BirthdayForm);
+        expect(screen.queryByRole("button")).toBeVisible();
+    });
+    it("has a input Name", () => {
+        render(BirthdayForm);
+        expect(screen.queryByLabelText("Name", {
+            selector: 'input[type=text]'
+        })
+        ).toBeVisible();
+    });
+    it("has a input dob", () => {
+        render(BirthdayForm);
+        expect(screen.queryByLabelText("dob", {
+            selector: 'input[type=date]'
+        })
+        ).toBeVisible();
+    });
 });
